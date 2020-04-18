ac9a32452ca0c275e7695f7ab6341fe669eb65dc
Akos Somodi <akossomodi@yahoo.com>
2020-04-18 15:56:55 +0200

    use sine and cosine for car driving

--- Driving_Game/script.js
+++ Driving_Game/script.js
@@ -4,8 +4,7 @@ var carPicLoaded = false;
 var carX = 75;
 var carY = 75;
 var carAng = 0;
-var carSpeedX = 5;
-var carSpeedY = 7;
+var carSpeed = 2;
 
 const TRACK_W = 40;
 const TRACK_H = 40;
@@ -87,8 +86,8 @@ function carReset() {
 }
 
 function carMove() {
-  // carX += carSpeedX;
-  // carY += carSpeedY;
+  carX += Math.cos(carAng) * carSpeed;
+  carY += Math.sin(carAng) * carSpeed;
   carAng += 0.02;
 
   if (carX < 0 && carSpeedX < 0.0) {
