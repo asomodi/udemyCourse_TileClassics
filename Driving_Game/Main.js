e7c30498a2670676ec7bddec4b8a2610e06747a6
Akos Somodi <akossomodi@yahoo.com>
2020-04-21 20:26:01 +0200

    load and display the tile images

--- Driving_Game/Main.js
+++ Driving_Game/Main.js
@@ -9,6 +9,7 @@ window.onload = function () {
 
   setupInput();
 
+  trackLoadImages();
   carImageLoad();
 
   carReset();
@@ -30,6 +31,6 @@ function clearScreen() {
 
 function drawAll() {
   clearScreen();
-  carDraw();
   drawTracks();
+  carDraw();
 }
