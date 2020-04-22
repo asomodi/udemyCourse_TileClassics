57e8aac1ed7d5e6a9b9ab54caabc0bd2cf90b5ad
Akos Somodi <akossomodi@yahoo.com>
2020-04-22 11:57:20 +0200

    waiting for the last image to load

--- Driving_Game/imageLoading.js
+++ Driving_Game/imageLoading.js
@@ -1,17 +1,25 @@
 var carPic = document.createElement('img');
-var carPicLoaded = false;
-
 var wallPic = document.createElement('img');
 var roadPic = document.createElement('img');
 
+var picsToLoad = 3;
+
+function countLoadedImagesAndLAunchIfReady() {
+  picsToLoad--;
+  // console.log(picsToLoad);
+  if (picsToLoad == 0) {
+    imageLoadingDoneSoStartGame();
+  }
+}
+
 function carImageLoad() {
-  carPic.onload = function () {
-    carPicLoaded = true;
-  };
+  carPic.onload = countLoadedImagesAndLAunchIfReady;
   carPic.src = 'player1car.png';
 }
 
 function trackLoadImages() {
+  roadPic.onload = countLoadedImagesAndLAunchIfReady;
+  wallPic.onload = countLoadedImagesAndLAunchIfReady;
   roadPic.src = 'track_road.png';
   wallPic.src = 'track_wall.png';
 }
